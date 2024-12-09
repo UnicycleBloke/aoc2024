d7b1dbdbe271aee5ee71369b3b404bb636b6cea9
Al Chambers <unicycle.bloke@gmail.com>
2024-12-09 06:46:05 +0000

    Working on Day 9

--- day07/solution.cpp
+++ day07/solution.cpp
@@ -3,8 +3,14 @@
 
 struct Equation
 {
-    uint64_t result;
-    vector<uint64_t> operands;
+    uint64_t result{};
+    vector<uint64_t> operands{};
+
+    Equation() = default;
+    Equation(const Equation&) = delete;
+    Equation& operator=(const Equation&) = delete;
+    Equation(Equation&&) = default;
+    Equation& operator=(Equation&&) = default;
 };
 
 
@@ -47,6 +53,34 @@ bool could_be_true2(const Equation& e, uint64_t result, int index)
 }
 
 
+
+// Recursive trinary search.
+bool could_be_true3(uint64_t target, const uint64_t* operands, int numops, uint64_t result, int index)
+{
+    // Condition reduces part2 execution time.
+    if (result > target) return false;
+
+    if (index == numops)
+        return target == result;
+    
+    // Return early rather than evaluate further. Cheaper operator first.
+    if (could_be_true3(target, operands, numops, result + operands[index], index + 1)) return true;
+    if (could_be_true3(target, operands, numops, result * operands[index], index + 1)) return true;
+
+    // Concatentation operator.
+    auto temp = operands[index];
+    while (temp > 0)
+    {
+        result *= 10;
+        temp   /= 10;
+    }
+    return could_be_true3(target, operands, numops, result + operands[index], index + 1);
+}
+
+
+
+
+
 template <typename T>
 auto part1(const T& input)
 {
@@ -76,6 +110,145 @@ auto part2(T& input)
     return total;
 }
 
+template <typename T>
+auto part3(T& input)
+{
+    aoc::timer timer;
+
+    uint64_t total{};
+    for (const auto& e: input)
+    {
+        if (could_be_true3(e.result, &e.operands[0], e.operands.size(), e.operands[0], 1)) total += e.result;
+    }
+
+    return total;
+}
+
+
+#include <span>
+
+// bool is_valid(uint64_t result, const span<const uint64_t>& operands)     
+// {
+//     auto l = operands.size();
+//     if (l == 1) 
+//     {
+//         return result == operands[0];
+//     }
+//     auto last = operands[l - 1];
+
+//     if (result % last == 0) 
+//     {
+//         if (is_valid(result / last, span<const uint64_t>(operands.begin(), operands.end() - 1)))
+//         {
+//             return true;
+//         }
+//     }
+
+//     auto sub = result - last;
+//     if (sub >= 0) 
+//     {
+//         return is_valid(sub, span<const uint64_t>(operands.begin(), operands.end() - 1));
+//     }
+
+//     return false;
+// }
+
+
+// bool is_valid2(uint64_t result, span<const uint32_t> operands)     
+// {
+//     auto l = operands.size();
+//     if (l == 1) 
+//         return result == operands[0];
+
+//     uint32_t last = operands[l - 1];
+//     if (result % last == 0) 
+//         if (is_valid2(result / last, span<const uint32_t>(operands.begin(), operands.end() - 1)))
+//             return true;
+
+//     auto sub = result - last;
+//     if (sub >= 0) 
+//     {
+//         if (is_valid2(sub, span<const uint32_t>(operands.begin(), operands.end() - 1)))
+//             return true;
+
+//         auto p = 10;       
+//         uint32_t temp = last;
+//         while (temp >= 10)
+//         {
+//             p    *= 10;
+//             temp /= 10;
+//         }
+//         if (sub % p == 0) 
+//         {
+//             if (is_valid2(sub / p, span<const uint32_t>(operands.begin(), operands.end() - 1)))
+//                 return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+
+// uint64_t part1_rust(const vector<Equation>& equations) 
+// {
+//     aoc::timer timer;
+
+//     uint64_t total{};
+//     for (const auto& [result, operands]: equations)
+//     {
+//         if (is_valid(result, span<const uint64_t>{operands})) total += result;
+//     }
+//     return total;
+// }
+
+
+// uint64_t part2_rust(const vector<Equation>& equations) 
+// {
+//     aoc::timer timer;
+
+//     uint64_t total{};
+//     for (const auto& [result, operands]: equations)
+//     {
+//         if (is_valid2(result, span<const uint32_t>{operands})) total += result;
+//     }
+//     return total;
+// }
+
+
+// template <typename T>
+// auto part1_matt(const T& input)
+// {
+//     aoc::timer timer;
+
+//     uint64_t result{};
+
+//     for (const auto& e: input)
+//     {
+//         int      length = e.operands.size();
+//         int      count  = 1 << length; 
+
+//         for (int p = 0; p < count; ++p)
+//         {
+//             uint64_t output = e.operands[0];
+            
+//             for (int i = 1; i < length; ++i)
+//             {
+//                 int q = 1 << (i - 1);
+//                 output = (p & q) == 0 ? output * e.operands[i] : output + e.operands[i];
+//                 if (output > e.result) break;
+//             }
+
+//             if (output == e.result)
+//             {
+//                 result += output;
+//                 break;
+//             }
+//         }
+//     }
+
+//     return result;
+// }
+
 
 void run(const char* filename)
 {
@@ -88,22 +261,33 @@ void run(const char* filename)
         Equation e;
         e.result = stoll(s[0]);
         e.operands = aoc::make_vector<uint64_t>(s[1], " ");
-        equations.push_back(e);
+        equations.push_back(move(e));
     }
 
     auto p1 = part1(equations);
     cout << "Part1: " << p1 << '\n';
-    
+
+    //auto p1r = part1_rust(equations);
+    //cout << "Part1 Rust: " << p1r << '\n';
+
+    //auto p1m = part1_matt(equations);
+    //cout << "Part1 Matt: " << p1m << '\n';
+
     auto p2 = part2(equations);
     cout << "Part2: " << p2 << '\n';
+
+    auto p2b = part3(equations);
+    cout << "Part2b: " << p2b << '\n';
    
+    //auto p2r = part2(equations);
+    //cout << "Part2 Rust: " << p2r << '\n';
 }
 
 
 int main(int argc, char** argv)
 {
     aoc::timer timer;
-    try
+    //try
     {
         if (argc < 2)
         {
@@ -120,8 +304,8 @@ int main(int argc, char** argv)
 
         run(argv[1]);
     }
-    catch (std::exception& e)
-    {
-        cout << e.what();
-    }
+    // catch (std::exception& e)
+    // {
+    //     cout << e.what();
+    // }
 }
--- day09/solution.cpp
+++ day09/solution.cpp
@@ -5,52 +5,166 @@ template <typename T>
 auto part1(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    int         file{};    
+    vector<int> blocks;
+
+    size_t pos{};
+    while (pos < input[0].size())
+    {
+        auto len = input[0][pos] - '0';
+        if ((pos % 2) == 0)
+        {
+            for (auto i: aoc::range(len)) blocks.push_back(file); 
+            ++file;
+        }
+        else
+        {
+           for (auto i: aoc::range(len)) blocks.push_back(-1);
+        }
+        ++pos; 
+    }
+
+    // for (auto i: blocks)
+    //     cout << i;
+    // cout << endl;
+
+    int beg  = 0;
+    int end  = blocks.size() - 1;
+    int size = blocks.size();
+
+    //while ((end > 0) && (beg < size))
+    while (end > beg)
+    {
+        while ((beg < size) && (blocks[beg] != -1))
+            ++beg;            
+        while ((end > 0) && (blocks[end] == -1))
+            --end;
+
+        while ((blocks[beg] == -1) && (blocks[end] != -1))
+        {
+            int temp    = blocks[beg];
+            blocks[beg] = blocks[end];
+            blocks[end] = temp;
+            ++beg;
+            --end;
+        }
+    }
+
+    // cout << "b=" << beg << " e=" << end << endl;
+    // cout << "b=" << blocks[beg] << " e=" << blocks[end] << endl;
+    // cout << "_123456789_123456789_123456789_123456789_123456789" << endl;
+
+    // for (auto i: blocks)
+    //     cout << i;
+    // cout << endl;
+
+    // for (auto i: aoc::range(blocks.size()))
+    // {
+    //     if (blocks[i] == -1) 
+    //         cout << '.';
+    //     else 
+    //         cout << blocks[i];
+    // }
+    // cout << endl;
+
+
+    int64_t result{};
+    for (auto i: aoc::range(blocks.size()))
+    {
+        if (blocks[i] == -1) break;
+        result += i * blocks[i]; 
+    } 
+
+    return result;
 }
+
+
+struct Block
+{
+    int id; // -1 is free;
+    int size;
+};
 
 
 template <typename T>
 auto part2(T& input)
 {
     aoc::timer timer;
+
+    int         file{};    
+    list<Block> blocks;
+
+    size_t pos{};
+    while (pos < input[0].size())
+    {
+        auto len = input[0][pos] - '0';
+        if ((pos % 2) == 0)
+        {
+            blocks.push_back(Block{file, len});
+            ++file;
+        }
+        else
+        {
+            blocks.push_back(Block{-1, len});
+        }
+        ++pos; 
+    }
+
+    for (const auto& b: blocks)
+    {
+        cout << b.id << " " << b.size << endl; 
+    }
+
+
+    
+    //while (true)
+    for (auto i: aoc::range(3))
+    {
+        auto rev = blocks.rbegin();
+        while ((rev != blocks.rend()) && (rev->id == -1))
+            rev++;
+        if (rev == blocks.rend()) break;
+        cout << "rev " << rev->id << " " << rev->size << endl;
+
+        auto fwd = blocks.begin();
+        while ((fwd != blocks.end()) && (fwd->id != -1))// && (fwd->size < rev->size))
+            fwd++;
+        if (fwd == blocks.end()) break;
+        cout << "fwd " << fwd->id << " " << fwd->size << endl;
+
+        Block file = *rev;
+        fwd->size -= rev->size;
+        rev->id    = -1;
+        blocks.insert(fwd, file);
+    }
+
+    for (const auto& b: blocks)
+    {
+        cout << b.id << " " << b.size << endl; 
+    }
+
     return 0;
 }
 
 
-// vector<tuple<Args...>>
-//auto lines = aoc::read_lines<int,int,int,int>(filename, R"((\d+)-(\d+),(\d+)-(\d+))");
-
-// vector<string>    
-//auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
-
-// Replace all substrings matching "search" with "replace".
-//std::string replace(std::string source, const std::string& search, const std::string& replace);
-
-// Split a delimited string of tokens into a vector of string tokens. Trims substrings by default and drops trimmed 
-// tokens which are empty by default. Not convinced how useful the option are, but you never know.
-//std::vector<std::string> split(std::string source, std::string delim, Blanks allow_blanks = Blanks::Suppress, Trim trim_subs = Trim::Yes);
-
-// vector (size_type n, const value_type& val = value_type(),
-//vector<int> row(COLS, 0);
-//vector<vector<int>> grid(ROWS, row);
 void run(const char* filename)
 {
     auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
+    //cout << lines[0].size();
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
     
-
     auto p2 = part2(lines);
     cout << "Part2: " << p2 << '\n';
-    
 }
 
 
 int main(int argc, char** argv)
 {
     aoc::timer timer;
-    try
+    //try
     {
         if (argc < 2)
         {
@@ -67,8 +181,8 @@ int main(int argc, char** argv)
 
         run(argv[1]);
     }
-    catch (std::exception& e)
-    {
-        cout << e.what();
-    }
+    // catch (std::exception& e)
+    // {
+    //     cout << e.what();
+    // }
 }
