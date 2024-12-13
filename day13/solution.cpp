5a92025cd213e9062d93d5d9605681a6ce595c81
Al Chambers <unicycle.bloke@gmail.com>
2024-12-13 05:49:38 +0000

    Solved Day 13

--- day13/solution.cpp
+++ day13/solution.cpp
@@ -1,49 +1,78 @@
 #include "utils.h"
 
 
-template <typename T>
-auto part1(const T& input)
+struct Game
 {
-    aoc::timer timer;
-    return 0;
+    int64_t ax;
+    int64_t ay;
+    int64_t bx;
+    int64_t by;
+    int64_t px;
+    int64_t py;
+};
+ 
+
+auto run_games(const vector<Game>& games, int64_t offset)
+{
+    int64_t total = 0; 
+
+    // Each game involve solving a pair of simultaneous equations. So glad I took a moment to 
+    // write down expressions for the final position. Immediately spotted the SE and did not 
+    // waste time on the suggested simulation. Would have killed P2.
+    //    
+    // There is a unique solution but it may involve a non-integral number of moves for A or B.
+    // We ignore those ones. I didn't explicity check for division by zero. Was expecting a nasty 
+    // edge case but seem to have got away with it.
+    for (const auto& [ax, ay, bx, by, prizex, prizey]: games)
+    {
+        auto px = prizex + offset;
+        auto py = prizey + offset;
+
+        auto num = px * ay - py * ax;
+        auto den = bx * ay - by * ax;
+
+        if ((num % den) == 0)
+        {
+            auto move_b = num / den;
+            auto num    = px  - move_b * bx;
+            if ((num % ax) == 0)
+            {
+                auto move_a = num / ax;
+
+                total += move_a * 3 + move_b; 
+            }
+        }
+    }
+
+    return total;
 }
 
 
-template <typename T>
-auto part2(T& input)
-{
-    aoc::timer timer;
-    return 0;
-}
-
-
-// vector<tuple<Args...>>
-//auto lines = aoc::read_lines<int,int,int,int>(filename, R"((\d+)-(\d+),(\d+)-(\d+))");
-
-// vector<string>    
-//auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
-
-// Replace all substrings matching "search" with "replace".
-//std::string replace(std::string source, const std::string& search, const std::string& replace);
-
-// Split a delimited string of tokens into a vector of string tokens. Trims substrings by default and drops trimmed 
-// tokens which are empty by default. Not convinced how useful the option are, but you never know.
-//std::vector<std::string> split(std::string source, std::string delim, Blanks allow_blanks = Blanks::Suppress, Trim trim_subs = Trim::Yes);
-
-// vector (size_type n, const value_type& val = value_type(),
-//vector<int> row(COLS, 0);
-//vector<vector<int>> grid(ROWS, row);
 void run(const char* filename)
 {
-    auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
+    auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress);
+    
+    vector<Game> games;
 
-    auto p1 = part1(lines);
+    size_t i{};
+    while (i < lines.size())
+    {
+        auto a = aoc::parse_line<int64_t, int64_t>(R"(Button A: X\+(\d+), Y\+(\d+))", lines[i++]);
+        auto b = aoc::parse_line<int64_t, int64_t>(R"(Button B: X\+(\d+), Y\+(\d+))", lines[i++]);
+        auto p = aoc::parse_line<int64_t, int64_t>(R"(Prize: X=(\d+), Y=(\d+))", lines[i++]);
+
+        auto [ax, ay] = a;
+        auto [bx, by] = b;
+        auto [px, py] = p;
+
+        games.push_back(Game{ax, ay, bx, by, px, py});
+    }
+
+    auto p1 = run_games(games, 0);
     cout << "Part1: " << p1 << '\n';
     
-
-    auto p2 = part2(lines);
+    auto p2 = run_games(games, 10000000000000);
     cout << "Part2: " << p2 << '\n';
-    
 }
 
 
