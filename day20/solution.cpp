1c5f37ffb4623fa2758820d9bc6001f93f91ef7b
Al Chambers <unicycle.bloke@gmail.com>
2024-12-20 06:58:00 +0000

    Day20 Part1 solved.

--- day20/solution.cpp
+++ day20/solution.cpp
@@ -1,11 +1,140 @@
 #include "utils.h"
 
 
+int run(vector<string> grid, int tick_cheat, char dir_cheat)
+{
+    //aoc::timer timer;
+
+    int Rows = grid.size();
+    int Cols = grid[0].size();
+
+    int rs, cs, re, ce;
+
+    for (auto r: aoc::range(Rows))
+    {
+        for (auto c: aoc::range(Cols))
+        {
+            if (grid[r][c] == 'S') 
+            {
+                rs = r; cs = c;                
+            }
+            if (grid[r][c] == 'E') 
+            {
+                re = r; ce = c;                
+            }
+        }
+    }
+
+    set<pair<int, int>>      visited;
+    set<pair<int, int>>      front;
+    map<pair<int, int>, int> costs;
+
+    front.insert({rs, cs});
+    costs[{rs, cs}] = 0;
+
+    int tick = -1;
+    while (front.size() > 0)
+    {        
+        ++tick;
+
+        set<pair<int, int>> front2;
+        for (auto [r, c]: front)
+        {
+            if ((r == re) && (c == ce)) 
+                return tick;
+
+            visited.insert({r, c});
+
+            if ((grid[r][c] == '#'))
+                continue;
+
+            if (tick == tick_cheat)
+            {
+                switch (dir_cheat)
+                {
+                    case 'N': if (r > 0)          grid[r - 1][c] = '.'; break;
+                    case 'S': if (r < (Rows - 1)) grid[r + 1][c] = '.'; break;
+                    case 'E': if (c < (Cols - 1)) grid[r][c + 1] = '.'; break;
+                    case 'W': if (c > 0)          grid[r][c - 1] = '.'; break;
+                }
+            }
+
+
+            // //bool can_cheat = (tick == tick_cheat) || (tick == (tick_cheat + 1));
+            // //bool can_cheat = (tick == (tick_cheat + 1));
+            // bool can_cheat = (tick == tick_cheat);
+            // if ((grid[r][c] == '#') && !can_cheat)
+            //     continue;
+
+            int steps = costs[{r, c}] + 1;
+            //cout << r << " " << c << " " << grid[r][c] << steps << endl;
+
+            auto make_step = [&](int dr, int dc)
+            { 
+                if (costs.find({r + dr, c + dc}) == costs.end())
+                    costs[{r + dr, c + dc}] = steps;
+                else
+                    costs[{r + dr, c + dc}] = min(steps, costs[{r + dr, c + dc}]);
+
+                int r2 = r + dr; 
+                int c2 = c + dc; 
+                if ((r2 == 0) || (c2 == 0) || (r2 == (Rows - 1)) || (c2 == (Cols - 1))) return;    
+
+                if (visited.find({r + dr, c + dc}) == visited.end()) front2.insert({r + dr, c + dc});
+            };
+
+            make_step( 1,  0);
+            make_step(-1,  0);
+            make_step( 0,  1);
+            make_step( 0, -1);
+        }
+
+        front = front2;
+    }
+
+    return tick;
+}
+
+
 template <typename T>
 auto part1(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    int result = 0;
+    int base   = run(input, -2, 'N') - 99;
+    cout << base << endl;
+
+    map<int, int> saves;
+    for (auto t: aoc::range(0, base))
+    {        
+        cout << t << endl;
+
+        {
+            int tick = run(input, t, 'N');
+            saves[base - tick] = saves[base - tick] + 1;
+            result += (base > tick);
+        }
+        {
+            int tick = run(input, t, 'S');
+            saves[base - tick] = saves[base - tick] + 1;
+            result += (base > tick);
+        }
+        {
+            int tick = run(input, t, 'E');
+            saves[base - tick] = saves[base - tick] + 1;
+            result += (base > tick);
+        }
+        {
+            int tick = run(input, t, 'W');
+            saves[base - tick] = saves[base - tick] + 1;
+            result += (base > tick);
+        }
+    }
+
+    for (auto [k, v]: saves) cout << k << " " << v << endl; cout << endl;
+
+    return result;
 }
 
 
@@ -17,33 +146,15 @@ auto part2(T& input)
 }
 
 
-// vector<tuple<Args...>>
-//auto lines = aoc::read_lines<int,int,int,int>(filename, R"((\d+)-(\d+),(\d+)-(\d+))");
-
-// vector<string>    
-//auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
-
-// Replace all substrings matching "search" with "replace".
-//std::string replace(std::string source, const std::string& search, const std::string& replace);
-
-// Split a delimited string of tokens into a vector of string tokens. Trims substrings by default and drops trimmed 
-// tokens which are empty by default. Not convinced how useful the option are, but you never know.
-//std::vector<std::string> split(std::string source, std::string delim, Blanks allow_blanks = Blanks::Suppress, Trim trim_subs = Trim::Yes);
-
-// vector (size_type n, const value_type& val = value_type(),
-//vector<int> row(COLS, 0);
-//vector<vector<int>> grid(ROWS, row);
 void run(const char* filename)
 {
     auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
 
     auto p1 = part1(lines);
-    cout << "Part1: " << p1 << '\n';
-    
+    cout << "Part1: " << p1 << '\n';   
 
     auto p2 = part2(lines);
     cout << "Part2: " << p2 << '\n';
-    
 }
 
 
