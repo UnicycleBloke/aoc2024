356f255d22ca34df4939e955188f3561ca2a7e1d
Al Chambers <unicycle.bloke@gmail.com>
2024-12-22 06:56:42 +0000

    Solved Day 22.

--- day22/solution.cpp
+++ day22/solution.cpp
@@ -5,45 +5,84 @@ template <typename T>
 auto part1(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    uint64_t result = 0;
+    for (auto [secret]: input)
+    {
+        for (auto i: aoc::range(2000))
+        {
+            auto next = secret * 64;
+            secret = (secret ^ next) % 16777216;
+            next = secret / 32;
+            secret = (secret ^ next) % 16777216;
+            next = secret * 2048;
+            secret = (secret ^ next) % 16777216;
+        }
+
+        result += secret;
+    }
+
+    return result;
 }
 
 
 template <typename T>
 auto part2(T& input)
 {
+    // It is convenient to represent the sequence of 4 changes as a single number. 
+    // Add 9 to the change to get a value in the range 0..18, which is base-19. 
+    // Store the sequence as a 4-digit base-19 number.
+    constexpr int kSequenceCap = 19 * 19 * 19 * 19;
+
     aoc::timer timer;
-    return 0;
+
+    array<int, kSequenceCap> total_prices{};
+    
+    for (auto [secret]: input)
+    {
+        int      price    = secret % 10;
+        int      sequence = 0;
+
+        // Use an array of bool to mark the sequences already seen for this buyer.
+        array<bool, kSequenceCap> sequences{};
+
+        for (auto i: aoc::range(2000))
+        {
+            auto next = secret * 64;
+            secret = (secret ^ next) % 16777216;
+            next = secret / 32;
+            secret = (secret ^ next) % 16777216;
+            next = secret * 2048;
+            secret = (secret ^ next) % 16777216;
+
+            int price2 = secret % 10;
+            int change = price2 - price;
+            price = price2;
+
+            // Store the sequence as a 4-digit base-19 number.
+            sequence = (sequence * 19 + change + 9) % kSequenceCap;
+
+            if (!sequences[sequence])
+            {
+                sequences[sequence] = true;                            
+                total_prices[sequence] += price;
+            }
+        }
+    }
+
+    return *max_element(total_prices.begin(), total_prices.end());
 }
 
 
-// vector<tuple<Args...>>
-//auto lines = aoc::read_lines<int,int,int,int>(filename, R"((\d+)-(\d+),(\d+)-(\d+))");
-
-// vector<string>    
-//auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
-
-// Replace all substrings matching "search" with "replace".
-//std::string replace(std::string source, const std::string& search, const std::string& replace);
-
-// Split a delimited string of tokens into a vector of string tokens. Trims substrings by default and drops trimmed 
-// tokens which are empty by default. Not convinced how useful the option are, but you never know.
-//std::vector<std::string> split(std::string source, std::string delim, Blanks allow_blanks = Blanks::Suppress, Trim trim_subs = Trim::Yes);
-
-// vector (size_type n, const value_type& val = value_type(),
-//vector<int> row(COLS, 0);
-//vector<vector<int>> grid(ROWS, row);
 void run(const char* filename)
 {
-    auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
+    auto lines = aoc::read_lines<uint32_t>(filename, R"((\d+))");
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
-    
-
+   
     auto p2 = part2(lines);
     cout << "Part2: " << p2 << '\n';
-    
 }
 
 
