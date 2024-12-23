0b61fa45b0bcc50926ca407f9f4b1a34bdb7349a
Al Chambers <unicycle.bloke@gmail.com>
2024-12-23 06:41:55 +0000

    Solved Day23 Part1

--- day23/solution.cpp
+++ day23/solution.cpp
@@ -5,45 +5,130 @@ template <typename T>
 auto part1(const T& input)
 {
     aoc::timer timer;
-    return 0;
+
+    map<string, vector<string>> neighbours;
+    for (auto [p1, p2]: input)
+    {        
+        neighbours[p1].push_back(p2);
+        neighbours[p2].push_back(p1);
+    }
+
+    set<set<string>> triples;
+    for (const auto& [p1, v1]: neighbours)     
+    {
+        if (p1[0] == 't')
+        {
+            auto s1 = v1.size();
+            for (auto i: aoc::range(s1))
+            {
+                const auto& p2 = v1[i];
+                const auto& v2 = neighbours[p2];
+                for (auto j: aoc::range(i, s1))
+                {
+                    const auto& p3 = v1[j];
+                    if (find(v2.begin(), v2.end(), p3) != v2.end())
+                    {
+                        triples.insert({p1, p2, p3});
+                    }
+                }
+            }
+        }
+    }
+
+    return triples.size();
+}
+
+
+bool is_cluster(const set<string>& cluster, const string& extra, map<string, vector<string>>& neighbours)
+{
+    const auto& v = neighbours[extra];
+    for (const auto& p: cluster)
+    {
+        if (find(v.begin(), v.end(), p) == v.end()) return false;
+    }
+
+    return true;
+}
+
+set<string> max_cluster;
+
+void find_cluster(const set<string>& cluster, map<string, vector<string>>& neighbours)
+{
+    cout << cluster.size() << endl;
+
+    if (cluster.size() > max_cluster.size())
+        max_cluster = cluster;
+
+    // Set of potential additions.
+    set<string> extra;
+    for (const auto& p: cluster)
+    {
+        for (const auto& p2: neighbours[p])
+        {
+            if (cluster.find(p2) == cluster.end()) extra.insert(p2);
+        }
+    }
+
+    for (const auto& e: extra)
+    {
+        if (is_cluster(cluster, e, neighbours))
+        {
+            auto cluster2 = cluster;
+            cluster2.insert(e);
+            find_cluster(cluster2, neighbours);
+        }
+    }
 }
 
 
 template <typename T>
 auto part2(T& input)
 {
-    aoc::timer timer;
-    return 0;
+    map<string, vector<string>> neighbours;
+    for (auto [p1, p2]: input)
+    {        
+        neighbours[p1].push_back(p2);
+        neighbours[p2].push_back(p1);
+    }
+
+    for (const auto& [p, v]: neighbours)     
+    {
+        set<string> cluster;
+        cluster.insert(p);
+        for (const auto& n: v) cluster.insert(n);
+
+        bool is_cluster = true;
+        for (const auto& n: v) 
+        {
+            const auto& v2 = neighbours[n];
+            for (const auto& n2: v2) 
+            {
+                if (n2 == n) continue;
+                is_cluster &= (find(v2.begin(), v2.end(), n2) != v2.end());
+            }
+        }
+
+        static int count{};
+        if (is_cluster)
+        {
+            cout << count++ << cluster.size() << ": ";
+            for (const auto& p: cluster) cout << p << ","; cout << endl;
+        }
+    }
+
+    return max_cluster.size();
 }
 
 
-// vector<tuple<Args...>>
-//auto lines = aoc::read_lines<int,int,int,int>(filename, R"((\d+)-(\d+),(\d+)-(\d+))");
-
-// vector<string>    
-//auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
-
-// Replace all substrings matching "search" with "replace".
-//std::string replace(std::string source, const std::string& search, const std::string& replace);
-
-// Split a delimited string of tokens into a vector of string tokens. Trims substrings by default and drops trimmed 
-// tokens which are empty by default. Not convinced how useful the option are, but you never know.
-//std::vector<std::string> split(std::string source, std::string delim, Blanks allow_blanks = Blanks::Suppress, Trim trim_subs = Trim::Yes);
-
-// vector (size_type n, const value_type& val = value_type(),
-//vector<int> row(COLS, 0);
-//vector<vector<int>> grid(ROWS, row);
 void run(const char* filename)
 {
-    auto lines = aoc::read_lines(filename, aoc::Blanks::Suppress); 
+    auto lines = aoc::read_lines<string, string>(filename, R"((\w+)-(\w+))");
 
     auto p1 = part1(lines);
     cout << "Part1: " << p1 << '\n';
-    
 
     auto p2 = part2(lines);
     cout << "Part2: " << p2 << '\n';
-    
 }
 
 
